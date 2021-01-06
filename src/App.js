6f6f10ca8ee280745691a3a84ae749f2397842e9
nageswarachandru <cybernag66@gmail.com>
2021-01-06 11:27:56 +0530

    init commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const app = () => {
+  return (
+    <>
+    <h1>Welcome to React Parcel Micro App!</h1>
+    <p>avengers assemble. LETS KILL THE THANOS THIS TIME PROPERLY.</p> 
+  </>
+  );
+};
+export default app;
