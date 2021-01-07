8684e583775db0581d669b4abaa74842ad12b17f
nageswarachandru <cybernag66@gmail.com>
2021-01-07 18:54:08 +0530

    Displayed game history

---
+++ src/components/History.js
@@ -0,0 +1,27 @@
+/* eslint-disable arrow-body-style */
+import React from 'react'
+
+// eslint-disable-next-line arrow-body-style
+const History = ({history,moveTo,currentMove}) => {
+  return (
+    <ul>
+      {history.map ((_, move) => {
+        return (
+          <li key = {move}>
+            <button style={{
+              fontWeight:  move === (currentMove) ? 'Bold' : 'Normal',
+            }}
+            type = "button" onClick = {() => {
+              moveTo(move);
+            }}>
+              {move === 0 ? 'Go lets start the game' : `Go to move #${move}`}
+              </button>
+            </li>
+        )
+      })}
+      
+    </ul>
+  )
+}
+
+export default History
