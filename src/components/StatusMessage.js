4cbe448e9ec6e8a92a235a8a6ec1a18614e44e7f
nageswarachandru <cybernag66@gmail.com>
2021-01-07 19:29:20 +0530

    displayed draw message with conditional rendering

---
+++ src/components/StatusMessage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// eslint-disable-next-line arrow-body-style
+const StatusMessage = ({winner,current}) => {
+  const noMovesLeft = current.board.every(el => el !==null);
+  return (
+    <h2>{winner && `winner is ${winner}` }
+    {!winner && !noMovesLeft && `Next player is ${current.isXNext ? 'X' : 'O'}`}
+    {!winner && noMovesLeft && 'X and O are tied'}
+    </h2>
+  )
+}
+
+export default StatusMessage
